1640bd436efaba53ff8f568fdad10c84b6f02770
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-04 01:05:50 +0530

    added access data demo

--- numpy_demo.py
+++ numpy_demo.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-mylist = [1,2,3]
-myarray = np.array(mylist)
-print(mylist)
+mylist = [[1, 2, 3], [3, 4, 5]]
+myarray = numpy.array(mylist)
 print(myarray)
 print(myarray.shape)
+print("First row: %s" % myarray[0])
+print("Last row: %s" % myarray[-1])
+print("Specific row and col: %s" % myarray[0, 2])
+print("Whole col: %s" % myarray[:, 2])
