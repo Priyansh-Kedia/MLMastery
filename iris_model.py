f915ed6b7fe6842619f0e76fdd436501566f9888
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-04 22:58:58 +0530

    added models comparison

--- iris_model.py
+++ iris_model.py
@@ -56,11 +56,45 @@ print(dataset.groupby('Species').size())
 
 
 # data visualisation
-dataset.plot(kind = 'box', subplots = True, layout = (2,2), sharex = False, sharey = False)
+# dataset.plot(kind = 'box', subplots = True, layout = (2,2), sharex = False, sharey = False)
+# plt.show()
+
+# dataset.hist()
+# plt.show()
+
+# scatter_matrix(dataset)
+# plt.show()
+
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+models = []
+models.append(('LR',LogisticRegression(solver='liblinear',multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+plt.boxplot(results, labels=names)
+plt.title("Algorithm comparison")
 plt.show()
 
-dataset.hist()
-plt.show()
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
 
-scatter_matrix(dataset)
-plt.show()
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
