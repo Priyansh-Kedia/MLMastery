59ef1dcec2ddf55101e639742bee0dee12f84b93
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-05 01:56:08 +0530

    added descriptive statictics

--- 5_descriptive_statistics.py
+++ 5_descriptive_statistics.py
@@ -3,6 +3,23 @@ import pandas as pd
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv"
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 data = pd.read_csv(url, names = names)
+
 print(data.head(20))
 print(data.shape)
 print(data.dtypes)
+
+description = data.describe()
+print(description)
+
+class_counts = data.groupby('class').size()
+print(class_counts)
+
+pd.set_option('display.width', 100)
+pd.set_option('precision', 3)
+correlations = data.corr(method='pearson')
+print(correlations)
+
+skew = data.skew()
+print(skew)
+# Skewness is a measure of the asymmetry of the probability 
+# distribution of a real-valued random variable about its mean
