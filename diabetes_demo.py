559d7e2db50d1e85f39b4118b0ff84b63bf511b9
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-05 00:33:02 +0530

    added complete diabetes model code

--- diabetes_demo.py
+++ diabetes_demo.py
@@ -1,22 +1,26 @@
-import pandas as pd
-from pandas.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-import numpy as np
-from sklearn.model_selection import KFold, cross_val_score
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
+import pickle
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-dataframe = pd.read_csv(url, names=names)
+dataframe = read_csv(url, names=names)
 array = dataframe.values
-# separate array into input and output components
 X = array[:,0:8]
 Y = array[:,8]
-
-
-# summarize tranformed data
-kfold = KFold(n_splits=10, random_state=7, shuffle=True)
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+# Fit the model on 67%
 model = LogisticRegression(solver='liblinear')
-results = cross_val_score(model, X, Y, cv=kfold)
-print("Accuracy: %.3f%% (%.3f%%)" % (results.mean()*100.0, results.std()*100.0))
+model.fit(X_train, Y_train)
+# save the model to disk
+filename = 'finalized_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+ 
+# some time later...
+ 
+# load the model from disk
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, Y_test)
+print(result)
