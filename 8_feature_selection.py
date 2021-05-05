c98f575a699013ec88629cd9ddaae045bb5cad4b
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-06 01:33:15 +0530

    added description for code

--- 8_feature_selection.py
+++ 8_feature_selection.py
@@ -20,6 +20,12 @@ Y = array[:,8]
 # feature extraction
 test = SelectKBest(score_func=f_classif, k = 4)
 fit = test.fit(X, Y)
+# Select features according to the k highest scores.
+# If k = 4, then this would select the 4 features which 
+# contribute the most to the output. score_func is the
+# function which is used to check what all features 
+# contribute the most
+
 # Selection of feature selection model depends on various
 # factors, it can be seen 
 # https://drive.google.com/file/d/1VuQDvpYipxJaadfgw3Fr-dYYvrbVwISq/view?usp=sharing
@@ -36,6 +42,16 @@ print(features[0:5,:])
 model = LogisticRegression(solver='lbfgs')
 rfe = RFE(model, 3)
 fit = rfe.fit(X, Y)
+# Feature ranking with recursive feature elimination.
+# The goal of recursive feature elimination (RFE) is to 
+# select features by recursively considering smaller and 
+# smaller sets of features. First, the estimator is trained 
+# on the initial set of features and the importance of each 
+# feature is obtained either through any specific attribute 
+# or callable. Then, the least important features are pruned 
+# from current set of features. That procedure is recursively 
+# repeated on the pruned set until the desired number of 
+# features to select is eventually reached.
 print("Num Features: %d" % fit.n_features_)
 print("Selected Features: %s" % fit.support_)
 print("Feature Ranking: %s" % fit.ranking_)
@@ -45,10 +61,17 @@ print("Feature Ranking: %s" % fit.ranking_)
 pca = PCA(n_components=3)
 fit = pca.fit(X)
 # summarize components
+# You can learn how PCA works from here
+# https://towardsdatascience.com/the-mathematics-behind-principal-component-analysis-fff2d7f4b643
 print("Explained Variance: %s" % fit.explained_variance_ratio_)
 print(fit.components_)
 
 # feature extraction
 model = ExtraTreesClassifier(n_estimators=10)
+# This class implements a meta estimator that 
+# fits a number of randomized decision trees 
+# (a.k.a. extra-trees) on various sub-samples 
+# of the dataset and uses averaging to improve 
+# the predictive accuracy and control over-fitting.
 model.fit(X, Y)
 print(model.feature_importances_)
