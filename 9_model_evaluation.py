2d840f30b479c9e401db54d5296f3bb8f60909a8
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-06 18:06:55 +0530

    added k fold cross validation

---
+++ 9_model_evaluation.py
@@ -0,0 +1,29 @@
+# Split into train test sets
+import pandas as pd
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pd.read_csv(filename, names = names)
+
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size = test_size, random_state=seed)
+
+# Split into train test sets
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+result = model.score(X_test, Y_test)
+print("Accuracy: %0.3f%%" % (result*100.0))
+
+# K-Fold Cross Validation
+seed = 7
+kfold = model_selection.KFold(n_splits=10, random_state=seed, shuffle=True)
+model = LogisticRegression()
+results = model_selection.cross_val_score(model, X, Y, cv = kfold)
+print("Accuracy: %.3f%% (%.3f%%)" % (results.mean()*100.0, results.std()*100.0))
+
