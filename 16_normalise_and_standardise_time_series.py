b909269f3a59970c550c2f97fee5280851d2575c
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-09 17:15:46 +0530

    added till scaling

--- 16_normalise_and_standardise_time_series.py
+++ 16_normalise_and_standardise_time_series.py
@@ -2,3 +2,17 @@
 # y = (x - min) / (max - min)
 
 
+from pandas import read_csv
+from sklearn.preprocessing import MinMaxScaler
+
+series = read_csv('daily-min-temperatures.csv', header=0, index_col=0)
+print(series.head())
+print(series.shape)
+
+values = series.values
+print(values)
+values = values.reshape((len(values), 1))
+
+# train the normalization
+scaler = MinMaxScaler(feature_range=(0,1))
+scaler = scaler.fit(values)
