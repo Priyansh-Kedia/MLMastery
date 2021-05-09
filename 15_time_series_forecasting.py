e970278d666c41df8019f37e94939baa8aa7e68f
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-09 16:34:07 +0530

    added time series forecasting

---
+++ 15_time_series_forecasting.py
@@ -0,0 +1,20 @@
+from pandas import read_csv
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+series = read_csv("daily-births.csv",header=0, parse_dates=[0], index_col=0, squeeze=True)
+# header=0: We must specify the header information at row 0.
+# parse_dates=[0]: We give the function a hint that data in 
+# the first column contains dates that need to be parsed. This 
+# argument takes a list, so we provide it a list of one element, 
+# which is the index of the first column.
+# index_col=0: We hint that the first column contains the index 
+# information for the time series.
+# squeeze=True: We hint that we only have one data column and that 
+# we are interested in a Series and not a DataFrame.
+
+print(type(series))
+print(series.head())
+
+
