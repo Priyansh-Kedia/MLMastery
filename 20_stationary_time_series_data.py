d749964ed0b7ef11f0b1b29a1e38a28abfe156fe
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-10 23:59:41 +0530

    added augmented dickey fuller

--- 20_stationary_time_series_data.py
+++ 20_stationary_time_series_data.py
@@ -35,3 +35,27 @@ mean1, mean2 = X1.mean(), X2.mean()
 var1, var2 = X1.var(), X2.var()
 print('mean1=%f, mean2=%f' % (mean1, mean2))
 print('variance1=%f, variance2=%f' % (var1, var2))
+
+
+# Augmented Dickey-Fuller test
+# The Augmented Dickey-Fuller test is a type of statistical test called a unit root test.
+# The intuition behind a unit root test is that it determines how strongly a time series 
+# is defined by a trend.
+
+# A p-value below a threshold (such as 5% or 1%) suggests we reject the null hypothesis 
+# (stationary), otherwise a p-value above the threshold suggests we fail to reject the null 
+# hypothesis (non-stationary). p-value > 0.05: Fail to reject the null hypothesis (H0), 
+# the data has a unit root and is non-stationary. p-value <= 0.05: Reject the null 
+# hypothesis (H0), the data does not have a unit root and is stationary.
+
+from statsmodels.tsa.stattools import adfuller
+
+series = read_csv('daily-total-female-births.csv', header=0, index_col=0, squeeze=True)
+X = series.values
+result = adfuller(X)
+print('ADF Statistic: %f' % result[0])
+print('p-value: %f' % result[1])
+print('Critical Values:')
+for key, value in result[4].items():
+	print('\t%s: %.3f' % (key, value))
+
