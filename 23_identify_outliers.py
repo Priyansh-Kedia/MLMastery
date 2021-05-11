d737fc14149624456d3c0707e75c83ff60545d3f
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-12 01:53:48 +0530

    added identify outliers

--- 23_identify_outliers.py
+++ 23_identify_outliers.py
@@ -56,3 +56,58 @@ print('Identified outliers: %d' % len(outliers))
 outliers_removed = [x for x in data if x >= lower and x <= upper]
 print('Non-outlier observations: %d' % len(outliers_removed))
 
+
+
+# Automatic outlier detection
+
+# The local outlier factor, or LOF for short, is a technique that attempts to harness 
+# the idea of nearest neighbors for outlier detection. Each example is assigned a scoring 
+# of how isolated or how likely it is to be outliers based on the size of its local neighborhood. 
+# Those examples with the largest score are more likely to be outliers.
+
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.neighbors import LocalOutlierFactor
+
+# load the dataset
+df = read_csv('boston-housing-dataset.csv', header=None)
+
+# retrieve the array
+data = df.values
+
+# split into input and output elements
+X, y = data[:, :-1], data[:, -1]
+
+# summarize the shape of the dataset
+print(X.shape, y.shape)
+
+# split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+# summarize the shape of the train and test sets
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, pred)
+print('MAE: %.3f' % mae)
+
+lof = LocalOutlierFactor()
+yhat = lof.fit_predict(X_train)
+# select all rows that are not outliers
+mask = yhat != -1
+X_train, y_train = X_train[mask, :], y_train[mask]
+# summarize the shape of the updated training dataset
+print(X_train.shape, y_train.shape)
+# fit the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+# evaluate the model
+yhat = model.predict(X_test)
+# evaluate predictions
+mae = mean_absolute_error(y_test, yhat)
+print('MAE: %.3f' % mae)
