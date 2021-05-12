55a94d3fcf63c0993641dff2046918623bc30352
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-12 16:07:56 +0530

    added sample model creation

--- 24_feature_importance.py
+++ 24_feature_importance.py
@@ -18,4 +18,19 @@ from sklearn.datasets import make_regression
 X_reg, Y_reg = make_regression(n_samples=1000, n_features=10, n_informative=5, random_state=seed)
 
 
+# Linear regression feature importance
+# We can fit a linear regression model on the data, and then get the coefficient values for the 
+# input features. This assumes that the input features had the same scale before training
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+
+model.fit(X_reg, Y_reg)
+
+importance = model.coef_
+
+for i,v in enumerate(importance):
+    print('Feature: %0d, Score: %.5f' % (i,v))
+
 
