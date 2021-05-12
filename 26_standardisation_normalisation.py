7509cbfa632a954772f6e7abf11176e4fdd3f1d6
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-12 17:47:59 +0530

    added min max transform

--- 26_standardisation_normalisation.py
+++ 26_standardisation_normalisation.py
@@ -66,3 +66,35 @@ cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
 # report model performance
 print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+
+
+from pandas import DataFrame
+
+data = dataset.values[:, :-1]
+scaler = MinMaxScaler()
+data = scaler.fit_transform(data)
+dataset = DataFrame(data)
+dataset.hist()
+plt.show()
+
+
+# MinMaxScaler transform
+
+from sklearn.pipeline import Pipeline
+
+dataset = read_csv('sonar.csv',header=None)
+data = dataset.values
+# separate into input and output columns
+X, y = data[:, :-1], data[:, -1]
+# ensure inputs are floats and output is an integer label
+X = X.astype('float32')
+y = LabelEncoder().fit_transform(y.astype('str'))
+# define the pipeline
+trans = MinMaxScaler()
+model = KNeighborsClassifier()
+pipeline = Pipeline(steps=[('t', trans), ('m', model)])
+# evaluate the pipeline
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+n_scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+# report pipeline performance
+print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
