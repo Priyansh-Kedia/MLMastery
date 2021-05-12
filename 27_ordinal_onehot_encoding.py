c2bfd28be22271d483b8f5aa37cfa535c827e303
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-12 18:02:51 +0530

    added one hot encoder

--- 27_ordinal_onehot_encoding.py
+++ 27_ordinal_onehot_encoding.py
@@ -24,3 +24,18 @@ encoder = OrdinalEncoder()
 # transform data
 result = encoder.fit_transform(data)
 print(result)
+
+
+# One hot encoding
+
+from numpy import asarray
+from sklearn.preprocessing import OneHotEncoder
+# define data
+data = asarray([['red'], ['green'], ['blue']])
+print(data)
+# define one hot encoding
+encoder = OneHotEncoder(sparse=False)
+# transform data
+onehot = encoder.fit_transform(data)
+print(onehot)
+
