d923f1cfb7be6c0ee55d7ce5dd1450e30e3c7599
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-12 17:49:46 +0530

    added standard scaler

--- 26_standardisation_normalisation.py
+++ 26_standardisation_normalisation.py
@@ -98,3 +98,45 @@ cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 n_scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
 # report pipeline performance
 print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+
+
+
+# Standard Scaler Transform
+
+from pandas import read_csv
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from matplotlib import pyplot
+# load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/sonar.csv"
+dataset = read_csv(url, header=None)
+data = dataset.values
+# separate into input and output columns
+X, y = data[:, :-1], data[:, -1]
+# ensure inputs are floats and output is an integer label
+X = X.astype('float32')
+y = LabelEncoder().fit_transform(y.astype('str'))
+# define the pipeline
+trans = StandardScaler()
+model = KNeighborsClassifier()
+pipeline = Pipeline(steps=[('t', trans), ('m', model)])
+# evaluate the pipeline
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+n_scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+# report pipeline performance
+print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+
+
+
+# If the distribution of the quantity is normal, then it should be standardized, otherwise, 
+# the data should be normalized. This applies if the range of quantity values is large 
+# (10s, 100s, etc.) or small (0.01, 0.0001).
+
+# It may be desirable to normalize data after it has been standardized.
+
+
+
