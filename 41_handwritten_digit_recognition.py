7849e93b850b61b06c4b3143f80ec571fe3e7b99
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-17 13:32:05 +0530

    added baseline model

--- 41_handwritten_digit_recognition.py
+++ 41_handwritten_digit_recognition.py
@@ -15,3 +15,25 @@ plt.subplot(224)
 plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
 # show the plot
 plt.show()
+
+# baseline model with a single hidden layer
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.utils import np_utils
+
+# load the mnist dataset
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data() 
+
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype("float32")
+X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype("float32")
+
+# rescaling the features, as we know the max value of pixel can be 255
+X_train = X_train / 255
+X_test = X_test / 255
+
+# Now we will use one hot encoding to transform the class integers to a binary matrix
+Y_train = np_utils.to_categorical(Y_train)
+Y_test = np_utils.to_categorical(Y_test)
+num_classes = Y_test.shape[1]
