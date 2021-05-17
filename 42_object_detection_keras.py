e4c9051b136959e23132b251034e263d8b294ac6
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-18 00:04:32 +0530

    added simple cnn

---
+++ 42_object_detection_keras.py
@@ -0,0 +1,63 @@
+from keras.datasets import cifar10
+from matplotlib import pyplot as plt
+
+(X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+for i in range(0,9):
+  plt.subplot(330 + 1 + i)
+  plt.imshow(X_train[i])
+
+plt.show()
+
+# Building a simple CNN
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# Normalize the data
+X_train = X_train.astype("float32")
+X_test = X_test.astype("float32")
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+Y_train = np_utils.to_categorical(Y_train)
+Y_test = np_utils.to_categorical(Y_test)
+num_classes = Y_test.shape[1]
+
+# Define model
+model = Sequential()
+# padding: one of "valid" or "same" (case-insensitive). "valid" means no padding. 
+# "same" results in padding with zeros evenly to the left/right or up/down of the 
+# input such that output has the same height/width dimension as the input.
+
+# kernel_constraint: Constraint function applied to the kernel matrix (see keras.constraints).
+
+# maxnorm - Constrains the weights incident to each hidden unit to have a norm less than or equal 
+# to a desired value.
+
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile model
+epochs = 25
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.summary()
+# Fit the model
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=epochs, batch_size=32)
+# Final evaluation of the model
+scores = model.evaluate(X_test, Y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
