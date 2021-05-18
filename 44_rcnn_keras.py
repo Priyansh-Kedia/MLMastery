b566f77c83216aa3ea70a59f8c139c0bb156a229
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-18 22:11:21 +0530

    minor change

--- 44_rcnn_keras.py
+++ 44_rcnn_keras.py
@@ -72,9 +72,9 @@ print(len(train), len(test))
 def create_dataset(dataset, look_back=1):
     dataX, dataY = [],[]
     for i in range(len(dataset) - look_back - 1):
-    a = dataset[i:(i+look_back),0]
-    dataX.append(a)
-    dataY.append(dataset[i + look_back, 0])
+        a = dataset[i:(i+look_back),0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
     return numpy.array(dataX), numpy.array(dataY)
 
 look_back = 1
