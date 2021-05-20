14849bc4d8c2f94176a810478c8f53797a89f969
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-20 15:48:46 +0530

    added baggin ensemble

--- 49_bagging_emsembles.py
+++ 49_bagging_emsembles.py
@@ -5,3 +5,15 @@ from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
 from sklearn.ensemble import BaggingClassifier
 
 X, Y = make_classification(random_state=1)
+
+# n_estimators specifies the number of trees to create
+model = BaggingClassifier(n_estimators=50)
+
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+# n_jobs: Number of jobs to run in parallel. When -1, uses all processors
+
+n_scores = cross_val_score(model, X, Y, scoring='accuracy', cv=cv, n_jobs=1)
+
+# report ensemble performance
+print('Mean Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
