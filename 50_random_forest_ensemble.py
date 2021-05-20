5c6aa4153795c03bf70e8edc5cf6bcf24f043d6f
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-20 15:54:57 +0530

    added random forest ensemble

---
+++ 50_random_forest_ensemble.py
@@ -0,0 +1,19 @@
+# Rather than considering all features when choosing a split point, 
+# random forest limits the features to a random subset of features, 
+# such as 3 if there were 10 features.
+
+from numpy import mean, std
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+
+X,Y = make_classification(random_state=1)
+
+model = RandomForestClassifier(n_estimators=50)
+
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+n_scores = cross_val_score(model, X, Y, scoring='accuracy', cv=cv, n_jobs=1)
+
+# report ensemble performance
+print('Mean Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
