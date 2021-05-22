f01e799fbd596a004c1f8d818bbace25133a28e4
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-22 22:32:44 +0530

    added replace missing values

--- 65_handle_missing_time_data.py
+++ 65_handle_missing_time_data.py
@@ -33,3 +33,40 @@ X,Y = generate_data(n_timesteps)
 # print sequence
 for i in range(len(X)):
 	print(X[i], '=>', Y[i])
+
+
+# Replace missing values
+
+from random import random
+from numpy import array
+from pandas import concat
+from pandas import DataFrame
+ 
+# generate a sequence of random values
+def generate_sequence(n_timesteps):
+	return [random() for _ in range(n_timesteps)]
+
+# generate data for the lstm
+def generate_data(n_timesteps):
+	# generate sequence
+	sequence = generate_sequence(n_timesteps)
+	sequence = array(sequence)
+	# create lag
+	df = DataFrame(sequence)
+	df = concat([df.shift(1), df], axis=1)
+	# replace missing values with -1
+	df.fillna(-1, inplace=True)
+	values = df.values
+	# specify input and output data
+	X, Y = values, values[:, 1]
+	return X, Y
+
+
+# generate sequence
+n_timesteps = 10
+
+X, Y = generate_data(n_timesteps)
+
+# print sequence
+for i in range(len(X)):
+	print(X[i], '=>', Y[i])
