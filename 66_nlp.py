e60d4e00a6d90742aad77886fac822115a6561a5
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-23 02:25:27 +0530

    added file read

---
+++ 66_nlp.py
@@ -0,0 +1,18 @@
+# Textual data must be cleaned first
+
+# Manual Tokenisation
+# Tokenisation: It is the process of turning raw
+# text into something which can be used to train
+# the model. The words after tokenisation are called tokens
+
+filename = "sample3.txt"
+file = open(filename,"rt")
+text = file.read()
+
+file.close()
+
+words = text.split()
+
+# convert to lowercase
+words = [word.lower() for word in words]
+print(words)
