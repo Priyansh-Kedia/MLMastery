7a8310b3a869156c8e899a83342f4cce10f5bb09
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-22 20:59:20 +0530

    added one hot encoding with sklearn

--- 63_one_hot_encoding.py
+++ 63_one_hot_encoding.py
@@ -25,3 +25,28 @@ print(onehot_encoded)
 # argmax: Returns the indices of the maximum values along an axis.
 inverted = int_to_char[argmax(onehot_encoded[0])]
 print(inverted)
+
+
+# One hot encoding using scikit-learn 
+from numpy import array, argmax
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# define example
+data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
+values = array(data)
+print(values)
+
+# encode values
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(values)
+print(integer_encoded)
+
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+print(onehot_encoded)
+
+# invert first example
+inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
+print(inverted)
