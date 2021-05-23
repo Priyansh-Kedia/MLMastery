f252dca1bb94c3746b9aebf38dca34d937a99a5d
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-23 16:16:18 +0530

    added bag of words sklearn

---
+++ 67_bag_of_words.py
@@ -0,0 +1,22 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+text = ["The quick brown fox jumped over the lazy dog.",
+		"The dog.",
+		"The fox"]
+
+# create the transform
+vectorizer = TfidfVectorizer()
+
+# tokenize and build vocab
+vectorizer.fit(text)
+
+# summarize
+print(vectorizer.vocabulary_)
+print(vectorizer.idf_)
+
+# encode document
+vector = vectorizer.transform([text[0]])
+
+# summarize encoded vector
+print(vector.shape)
+print(vector.toarray())
