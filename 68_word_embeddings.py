f16039dc2505ae5a8117dd4436a0d08be368ca10
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-23 16:50:44 +0530

    added word embeddings

---
+++ 68_word_embeddings.py
@@ -0,0 +1,22 @@
+from gensim.models import Word2Vec
+
+# define training data
+sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+			['this', 'is', 'the', 'second', 'sentence'],
+			['yet', 'another', 'sentence'],
+			['one', 'more', 'sentence'],
+			['and', 'the', 'final', 'sentence']]
+
+# train model
+model = Word2Vec(sentences, min_count=1)
+
+# summarize the loaded model
+print(model)
+
+# # summarize vocabulary
+# words = list(model.wv)
+
+# print(words)
+
+# access vector for one word
+print(model['sentence'])
