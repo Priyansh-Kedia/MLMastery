806d74e559a2f190d538fe4b779e3fa8d9a16328
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-24 16:21:23 +0530

    added positive and negative reviews to lines

--- 69_sentiment_analysis.py
+++ 69_sentiment_analysis.py
@@ -68,3 +68,40 @@ tokens = [k for k,c in vocab.items() if c >= min_occurane]
 print(len(tokens))
 
 save_list(tokens, "vocab.txt")
+
+
+def doc_to_line(filename, vocab):
+    # load the doc 
+    doc = load_doc(filename)
+    # clean doc
+    tokens = clean_doc(doc)
+    # filter according to vocab
+    tokens = [w for w in tokens if w in vocab]
+    return ' '.join(tokens)
+
+def docs_to_lines(directory, vocab):
+    lines = list()
+    # walk through all files in the folder
+    for filename in listdir(directory):
+        # skip any reviews in the test set
+        if filename.startswith('cv9'):
+            continue
+        # create the full path of the file to open
+        path = directory + '/' + filename
+        # load and clean the doc
+        line = doc_to_line(path, vocab)
+        # add to list
+        lines.append(line)
+    return lines
+
+# load the vocabulary
+vocab_filename = 'vocab.txt'
+vocab = load_doc(vocab_filename)
+vocab = vocab.split()
+vocab = set(vocab)
+
+# load all training reviews
+positive_lines = docs_to_lines('txt_sentoken/pos', vocab)
+negative_lines = docs_to_lines('txt_sentoken/neg', vocab)
+# summarize what we have
+print(len(positive_lines), len(negative_lines))
