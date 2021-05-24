dca47f1c45ffbbe4751b856dbff9112c767ca083
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-25 00:31:38 +0530

    word neural mode

---
+++ 71_word_based_neural_model.py
@@ -0,0 +1,128 @@
+from numpy import array
+from pickle import dump
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# load
+in_filename = 'republic_sequences.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+
+# integer encode sequences of words
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+# vocabulary size
+vocab_size = len(tokenizer.word_index) + 1
+
+# separate into input and output
+sequences = array(sequences)
+X, y = sequences[:,:-1], sequences[:,-1]
+y = to_categorical(y, num_classes=vocab_size)
+seq_length = X.shape[1]
+
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 50, input_length=seq_length))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(vocab_size, activation='softmax'))
+print(model.summary())
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit model
+model.fit(X, y, batch_size=128, epochs=100)
+
+# save the model to file
+model.save('model.h5')
+# save the tokenizer
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from random import randint
+from pickle import load
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# generate a sequence from a language model
+def generate_seq(model, tokenizer, seq_length, seed_text, n_words):
+	result = list()
+	in_text = seed_text
+	# generate a fixed number of words
+	for _ in range(n_words):
+		# encode the text as integer
+		encoded = tokenizer.texts_to_sequences([in_text])[0]
+		# truncate sequences to a fixed length
+		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+		# predict probabilities for each word
+		yhat = model.predict_classes(encoded, verbose=0)
+		# map predicted word index to word
+		out_word = ''
+		for word, index in tokenizer.word_index.items():
+			if index == yhat:
+				out_word = word
+				break
+		# append to input
+		in_text += ' ' + out_word
+		result.append(out_word)
+	return ' '.join(result)
+
+# load cleaned text sequences
+in_filename = 'republic_sequences.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+seq_length = len(lines[0].split()) - 1
+
+# load the model
+model = load_model('model.h5')
+
+# load the tokenizer
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+
+# select a seed text
+seed_text = lines[randint(0,len(lines))]
+print(seed_text + '\n')
+
+# generate new text
+generated = generate_seq(model, tokenizer, seq_length, seed_text, 50)
+print(generated)
