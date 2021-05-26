633e181e7ade82aae441ff57a1f74b4ce6162aea
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-26 12:29:38 +0530

    added local cenering

--- 79_scale_image_pixels.py
+++ 79_scale_image_pixels.py
@@ -51,3 +51,26 @@ pixels = pixels - mean
 mean = pixels.mean()
 print('Mean: %.3f' % mean)
 print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
+
+
+# Local centering (When image pixels are centered per channel)
+
+# load image
+image = Image.open('sydney_bridge.jpg')
+pixels = asarray(image)
+
+# convert from integers to floats
+pixels = pixels.astype('float32')
+
+# calculate per-channel means and standard deviations
+means = pixels.mean(axis=(0,1), dtype='float64')
+print('Means: %s' % means)
+print('Mins: %s, Maxs: %s' % (pixels.min(axis=(0,1)), pixels.max(axis=(0,1))))
+
+# per-channel centering of pixels
+pixels -= means
+
+# confirm it had the desired effect
+means = pixels.mean(axis=(0,1), dtype='float64')
+print('Means: %s' % means)
+print('Mins: %s, Maxs: %s' % (pixels.min(axis=(0,1)), pixels.max(axis=(0,1))))
