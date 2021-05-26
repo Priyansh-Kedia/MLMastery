46eaab5826d16d39efc2ab5e66362e2cacf06970
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-26 12:27:54 +0530

    added global cenetring

--- 79_scale_image_pixels.py
+++ 79_scale_image_pixels.py
@@ -28,3 +28,26 @@ pixels /= 255
 
 # confirm the normalization
 print("Min: %.3f, Max: %.3f" % (pixels.min(), pixels.max()))
+
+
+# Global centering (When image pixels are centered altogether)
+
+# load image
+image = Image.open('sydney_bridge.jpg')
+pixels = asarray(image)
+
+# convert from integers to floats
+pixels = pixels.astype('float32')
+
+# calculate global mean
+mean = pixels.mean()
+print('Mean: %.3f' % mean)
+print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
+
+# global centering of pixels
+pixels = pixels - mean
+
+# confirm it had the desired effect
+mean = pixels.mean()
+print('Mean: %.3f' % mean)
+print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
