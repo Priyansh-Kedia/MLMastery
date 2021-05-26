10978052bde5394392791b489ed7c6dc5dd77775
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-26 12:14:24 +0530

    added normalizattion

--- 79_scale_image_pixels.py
+++ 79_scale_image_pixels.py
@@ -10,3 +10,21 @@ print(image.size)
 
 # show the image
 image.show()
+
+# Normalize the pixel values
+from numpy import asarray
+
+pixels = asarray(image)
+
+# Some details about pixels
+print("Data Type %s" % pixels.dtype)
+print("Min: %.3f, Max: %.3f" % (pixels.min(), pixels.max()))
+
+# convert values from integers to floats
+pixels = pixels.astype("float32")
+
+# normalize the values in the range of 0-1
+pixels /= 255
+
+# confirm the normalization
+print("Min: %.3f, Max: %.3f" % (pixels.min(), pixels.max()))
