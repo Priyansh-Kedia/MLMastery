ef72cdf328cc192b84014d9ffb79b1f47c796948
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-26 12:34:37 +0530

    added positive standardization

--- 79_scale_image_pixels.py
+++ 79_scale_image_pixels.py
@@ -94,3 +94,38 @@ pixels = (pixels - mean) / std
 # confirm it had the desired effect
 mean, std = pixels.mean(), pixels.std()
 print('Mean: %.3f, Standard Deviation: %.3f' % (mean, std))
+
+
+# Positive Global standardization
+# There may be a desire to maintain the pixel values in the positive domain, 
+# perhaps so the images can be visualized or perhaps for the benefit of a 
+# chosen activation function in the model.
+# A popular way of achieving this is to clip the standardized pixel values 
+# to the range [-1, 1] and then rescale the values from [-1,1] to [0,1].
+
+from numpy import clip
+
+image = Image.open('sydney_bridge.jpg')
+pixels = asarray(image)
+
+# convert from integers to floats
+pixels = pixels.astype('float32')
+
+# calculate global mean and standard deviation
+mean, std = pixels.mean(), pixels.std()
+print('Mean: %.3f, Standard Deviation: %.3f' % (mean, std))
+
+# global standardization of pixels
+pixels = (pixels - mean) / std
+
+# clip pixel values to [-1,1]
+pixels = clip(pixels, -1.0, 1.0)
+
+# shift from [-1,1] to [0,1] with 0.5 mean
+pixels = (pixels + 1.0) / 2.0
+
+# confirm it had the desired effect
+mean, std = pixels.mean(), pixels.std()
+print('Mean: %.3f, Standard Deviation: %.3f' % (mean, std))
+print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
+
